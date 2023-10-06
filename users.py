20917a2e636706f509320c6adb05fcc89a7f5e18
Hxrshmxllow <86836424+Hxrshmxllow@users.noreply.github.com>
2023-10-06 15:37:49 +0000

    7

--- users.py
+++ users.py
@@ -3,6 +3,8 @@ import sqlite3
 def main(username, password):
     connection = sqlite3.connect('users.db')
     cursor = connection.cursor()
+    #cursor.execute("CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)")
 
-    cursor.execute('''INSERT INTO users VALUES(?,?,?)''', (1, username, password))
+    cursor.execute('''INSERT INTO users (username, password) VALUES(?,?)''', (username, password))
     connection.commit()
+
